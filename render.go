0f311daa63be8be0cafe0447112c5c62fe882252
pzrr@qq.com <pzrr@qq.com>
2017-03-28 11:11:54 +0800

    fixed issue #8 now can use like "{{.Url | unescaped}}" to view raw string, and we will add more default func to support template use.

--- render.go
+++ render.go
@@ -24,6 +24,9 @@ func (r *innerRenderer) Render(w io.Writer, tpl string, data interface{}, ctx *H
 	return t.Execute(w, data)
 }
 
+// 定义函数unescaped
+func unescaped(x string) interface{} { return template.HTML(x) }
+
 // return http/template by gived file name
 func parseFile(filename string) (*template.Template, error) {
 	var t *template.Template
@@ -34,11 +37,18 @@ func parseFile(filename string) (*template.Template, error) {
 	s := string(b)
 	name := filepath.Base(filename)
 	t = template.New(name)
+	t = registeTemplateFunc(t)
 	_, err = t.Parse(s)
 	if err != nil {
 		return nil, err
 	}
 	return t, nil
+}
+
+//registe default support funcs
+func registeTemplateFunc(t *template.Template) *template.Template {
+	return t.Funcs(template.FuncMap{"unescaped": unescaped})
+	//TODO:add more func
 }
 
 // NewInnerRenderer create a inner renderer instance
