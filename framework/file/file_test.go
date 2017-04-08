7021922d2cd3a71060c4982d219c1dbb54dae01f
cnphpbb <moqiruyi@gmail.com>
2017-04-08 15:35:18 +0800

    update file modules unil test file

--- framework/file/file_test.go
+++ framework/file/file_test.go
@@ -7,10 +7,29 @@ import (
 // 以下是功能测试
 
 func Test_GetCurrentDirectory_1(t *testing.T) {
-
+	thisDir := GetCurrentDirectory()
+	t.Log(thisDir)
 }
 
-func Test_GetFileExt_1(t *testing.T) {}
+func Test_GetFileExt_1(t *testing.T) {
+	fn := "/download/vagrant_1.9.2.dmg"
+	fileExt := GetFileExt(fn)
+	if len(fileExt) < 1 {
+		t.Error("fileExt null!")
+	} else {
+		t.Log(fileExt)
+	}
+}
+
+func Test_GetFileExt_2(t *testing.T) {
+	fn := "/download/vagrant_1"
+	fileExt := GetFileExt(fn)
+	if len(fileExt) < 1 {
+		t.Error("fileExt null!")
+	} else {
+		t.Log(fileExt)
+	}
+}
 
 func Test_Exist_1(t *testing.T) {}
 
