b8f2a3c11d6056fb18ba3442af85a7659ced95f0
cnphpbb <moqiruyi@gmail.com>
2017-04-08 15:22:35 +0800

    update file modules unil test file

--- framework/file/file_test.go
+++ framework/file/file_test.go
@@ -1,1 +1,17 @@
 package file
+
+import (
+	"testing"
+)
+
+// 以下是功能测试
+
+func Test_GetCurrentDirectory_1(t *testing.T) {
+
+}
+
+func Test_GetFileExt_1(t *testing.T) {}
+
+func Test_Exist_1(t *testing.T) {}
+
+// 以下是性能测试
