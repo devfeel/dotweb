fd2b6bd2ea8f8e30e7a69034143669a57048853c
zouyixian <zouyixian@kugou.net>
2017-06-28 18:28:11 +0800

    config包抽取公用方法

--- config/configs_test.go
+++ config/configs_test.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"testing"
+	"github.com/zouyx/agollo/test"
+)
+
+func TestInitConfig(t *testing.T) {
+	conf,err:=InitConfig("example/config/dotweb.json.conf","json")
+
+	test.Nil(t,err)
+	test.NotNil(t,conf)
+	test.NotNil(t,conf.App)
+	test.NotNil(t,conf.App.LogPath)
+	test.NotNil(t,conf.AppSets)
+	test.Equal(t,4,	len(conf.AppSets))
+}
+
+func TestInitConfigWithXml(t *testing.T) {
+	conf,err:=InitConfig("example/config/dotweb.conf","xml")
+
+	test.Nil(t,err)
+	test.NotNil(t,conf)
+	test.NotNil(t,conf.App)
+	test.NotNil(t,conf.App.LogPath)
+	test.NotNil(t,conf.AppSets)
+	test.Equal(t,4,	len(conf.AppSets))
+}
