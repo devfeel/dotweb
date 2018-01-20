ba4777b28d097f1c99fd327272b6fa445911b609
pzrr@qq.com <pxm@162413>
2018-01-20 23:54:07 +0800

    #### Version 1.4.5 * 新增yaml格式配置文件支持，具体参考 example/config/dotweb.yaml * config新增UnmarshalYaml\MarshalYaml\MarshalYamlString，提供针对Yaml的常规处理 * config新增UnmarshalXml\MarshalXml\MarshalXmlString，提供针对Xml的常规处理 * config新增UnmarshalJson\MarshalJson\MarshalJsonString，提供针对Json的常规处理 * UploadFile新增Bytes接口，用于返回上传文件本身 * 完善 example/config * 移除 example/session，查看更多示例，请移步https://github.com/devfeel/dotweb-example * 2018-01-20 23:40

--- config/config_json.go
+++ config/config_json.go
@@ -2,6 +2,22 @@ package config
 
 import "encoding/json"
 
-func fromJson(content []byte,v interface{}) error  {
+// UnmarshalJson parses the JSON-encoded data and stores the result
+// in the value pointed to by v.
+func UnmarshalJson(content []byte, v interface{}) error {
 	return json.Unmarshal(content, v)
+}
+
+// MarshalJson returns the JSON encoding of v.
+func MarshalJson(v interface{}) (out []byte, err error) {
+	return json.Marshal(v)
+}
+
+// MarshalJsonString returns the JSON encoding string format of v.
+func MarshalJsonString(v interface{}) (out string) {
+	marshal, err := json.Marshal(v)
+	if err != nil {
+		return ""
+	}
+	return string(marshal)
 }
--- config/config_xml.go
+++ config/config_xml.go
@@ -4,6 +4,24 @@ import (
 	"encoding/xml"
 )
 
-func fromXml(content []byte,v interface{}) error  {
+// UnmarshalXml parses the XML-encoded data and stores the result in
+// the value pointed to by v, which must be an arbitrary struct,
+// slice, or string. Well-formed data that does not fit into v is
+// discarded.
+func UnmarshalXml(content []byte, v interface{}) error {
 	return xml.Unmarshal(content, v)
+}
+
+// MarshalXml returns the XML encoding of v.
+func MarshalXml(v interface{}) (out []byte, err error) {
+	return xml.Marshal(v)
+}
+
+// MarshalXmlString returns the XML encoding string format of v.
+func MarshalXmlString(v interface{}) (out string) {
+	marshal, err := xml.Marshal(v)
+	if err != nil {
+		return ""
+	}
+	return string(marshal)
 }
