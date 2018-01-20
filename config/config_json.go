8ef96183f387013f549ac833f76050355730c49c
pzrr@qq.com <pxm@162413>
2018-01-21 00:09:45 +0800

    #### Version 1.4.5 * 新增yaml格式配置文件支持，具体参考 example/config/dotweb.yaml * config新增UnmarshalYaml\MarshalYaml\MarshalYamlString，提供针对Yaml的常规处理 * config新增UnmarshalXML\MarshalXML\MarshalXMLString，提供针对Xml的常规处理 * config新增UnmarshalJSON\MarshalJSON\MarshalJSONString，提供针对Json的常规处理 * UploadFile新增Bytes接口，用于返回上传文件本身 * 完善 example/config * 移除 example/session，查看更多示例，请移步https://github.com/devfeel/dotweb-example * 2018-01-20 23:40

--- config/config_json.go
+++ config/config_json.go
@@ -2,18 +2,18 @@ package config
 
 import "encoding/json"
 
-// UnmarshalJson parses the JSON-encoded data and stores the result
+// UnmarshalJSON parses the JSON-encoded data and stores the result
 // in the value pointed to by v.
 func UnmarshalJSON(content []byte, v interface{}) error {
 	return json.Unmarshal(content, v)
 }
 
-// MarshalJson returns the JSON encoding of v.
+// MarshalJSON returns the JSON encoding of v.
 func MarshalJSON(v interface{}) (out []byte, err error) {
 	return json.Marshal(v)
 }
 
-// MarshalJsonString returns the JSON encoding string format of v.
+// MarshalJSONString returns the JSON encoding string format of v.
 func MarshalJSONString(v interface{}) (out string) {
 	marshal, err := json.Marshal(v)
 	if err != nil {
--- config/config_xml.go
+++ config/config_xml.go
@@ -4,7 +4,7 @@ import (
 	"encoding/xml"
 )
 
-// UnmarshalXml parses the XML-encoded data and stores the result in
+// UnmarshalXML parses the XML-encoded data and stores the result in
 // the value pointed to by v, which must be an arbitrary struct,
 // slice, or string. Well-formed data that does not fit into v is
 // discarded.
@@ -12,12 +12,12 @@ func UnmarshalXML(content []byte, v interface{}) error {
 	return xml.Unmarshal(content, v)
 }
 
-// MarshalXml returns the XML encoding of v.
+// MarshalXML returns the XML encoding of v.
 func MarshalXML(v interface{}) (out []byte, err error) {
 	return xml.Marshal(v)
 }
 
-// MarshalXmlString returns the XML encoding string format of v.
+// MarshalXMLString returns the XML encoding string format of v.
 func MarshalXMLString(v interface{}) (out string) {
 	marshal, err := xml.Marshal(v)
 	if err != nil {
