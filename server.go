bd7acca37d72d77a730d611e50e6294a3b381fad
pzrr@qq.com <pzrr@qq.com>
2018-10-24 20:43:48 +0800

    #### Version 1.5.8 * New Feature: Add HttpServer.SetBinder, used to set custom Binder on HttpServer * Detail:   - Custom binder must implement dotweb.Binder interface * Example:   ``` golang   app.HttpServer.SetBinder(newUserBinder())   ``` * update example/bind * 2018-10-24 21:00

--- server.go
+++ server.go
@@ -102,6 +102,11 @@ func (server *HttpServer) SessionConfig() *config.SessionNode {
 	return server.DotApp.Config.Session
 }
 
+// SetBinder set custom Binder on HttpServer
+func (server *HttpServer) SetBinder(binder Binder){
+	server.binder = binder
+}
+
 // ListenAndServe listens on the TCP network address srv.Addr and then
 // calls Serve to handle requests on incoming connections.
 func (server *HttpServer) ListenAndServe(addr string) error {
