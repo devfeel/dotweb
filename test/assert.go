4de0237f0dee120e40e9acbd11328f6a830fa0bb
Yang Bo <yangbo12@huawei.com>
2018-12-19 16:23:32 +0800

    Add Contains method for test/assert
    Only strings are supported for now

--- test/assert.go
+++ test/assert.go
@@ -4,17 +4,26 @@ import (
 	"path/filepath"
 	"reflect"
 	"runtime"
+	"strings"
 	"testing"
 )
 
-func formattedLog(t *testing.T, fmt string, args ...interface{}) {
-	_, file, line, _ := runtime.Caller(2)
-	file = filepath.Base(file)
-	targs := make([]interface{}, len(args)+2)
-	targs[0] = file
-	targs[1] = line
-	copy(targs[2:], args)
-	t.Logf("\033[31m%s:%d:\n\n\t"+fmt+"\033[39m\n\n", targs...)
+func Contains(t *testing.T, expected, actual interface{}) {
+	if reflect.TypeOf(expected) != reflect.TypeOf(actual) {
+		formattedLog(t, "value type not match %T (expected)\n\n\t != %T (actual)", expected, actual)
+	}
+	switch e := expected.(type) {
+	case string:
+		a := actual.(string)
+		if !strings.Contains(a, e) {
+			formattedLog(t, "   %v (expected)\n\n\t!= %v (actual)",
+				expected, actual)
+			t.FailNow()
+		}
+	default:
+		formattedLog(t, "unsupported type %T(expected)", expected)
+		t.FailNow()
+	}
 }
 
 func Equal(t *testing.T, expected, actual interface{}) {
@@ -58,4 +67,14 @@ func isNil(object interface{}) bool {
 	}
 
 	return false
+}
+
+func formattedLog(t *testing.T, fmt string, args ...interface{}) {
+	_, file, line, _ := runtime.Caller(2)
+	file = filepath.Base(file)
+	targs := make([]interface{}, len(args)+2)
+	targs[0] = file
+	targs[1] = line
+	copy(targs[2:], args)
+	t.Logf("\033[31m%s:%d:\n\n\t"+fmt+"\033[39m\n\n", targs...)
 }
