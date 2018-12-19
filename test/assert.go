8c949aa07de3a9f315969e0a0c7bbb1449ec76f2
Yang Bo <yangbo12@huawei.com>
2018-12-19 15:34:58 +0800

    Re-organize logging for test/assert

--- test/assert.go
+++ test/assert.go
@@ -7,38 +7,41 @@ import (
 	"testing"
 )
 
+func formattedLog(t *testing.T, fmt string, args ...interface{}) {
+	_, file, line, _ := runtime.Caller(2)
+	file = filepath.Base(file)
+	targs := make([]interface{}, len(args)+2)
+	targs[0] = file
+	targs[1] = line
+	copy(targs[2:], args)
+	t.Logf("\033[31m%s:%d:\n\n\t"+fmt+"\033[39m\n\n", targs...)
+}
+
 func Equal(t *testing.T, expected, actual interface{}) {
 	if !reflect.DeepEqual(expected, actual) {
-		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\t   %#v (expected)\n\n\t!= %#v (actual)\033[39m\n\n",
-			filepath.Base(file), line, expected, actual)
+		formattedLog(t, "   %v (expected)\n\n\t!= %v (actual)",
+			expected, actual)
 		t.FailNow()
 	}
 }
 
 func NotEqual(t *testing.T, expected, actual interface{}) {
 	if reflect.DeepEqual(expected, actual) {
-		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\tvalue should not equal %#v\033[39m\n\n",
-			filepath.Base(file), line, actual)
+		formattedLog(t, "value should not equal %#v", actual)
 		t.FailNow()
 	}
 }
 
 func Nil(t *testing.T, object interface{}) {
 	if !isNil(object) {
-		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\t   <nil> (expected)\n\n\t!= %#v (actual)\033[39m\n\n",
-			filepath.Base(file), line, object)
+		formattedLog(t, "   <nil> (expected)\n\n\t!= %#v (actual)", object)
 		t.FailNow()
 	}
 }
 
 func NotNil(t *testing.T, object interface{}) {
 	if isNil(object) {
-		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\tExpected value not to be <nil>\033[39m\n\n",
-			filepath.Base(file), line, object)
+		formattedLog(t, "Expected value not to be <nil>", object)
 		t.FailNow()
 	}
 }
