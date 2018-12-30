ec4b19f311580d8159ef664485b8e7519d7b3062
mikewang <it512@msn.com>
2018-12-30 19:26:58 +0800

    http2 Push support

--- response_1.8.go
+++ response_1.8.go
@@ -4,6 +4,7 @@ package dotweb
 
 import "net/http"
 
+// support http2 Push
 func (r *Response) Push(target string, opts *http.PushOptions) error {
 	return r.writer.(http.Pusher).Push(target, opts)
 }
