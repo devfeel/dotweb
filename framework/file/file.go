d8d49004899bd35f0c22700583be3eae49d6b398
Abirdcfly <fp544037857@gmail.com>
2022-08-10 11:30:26 +0800

    delete minor unreachable code caused by log.Fatal
    Signed-off-by: Abirdcfly <fp544037857@gmail.com>

--- framework/file/file.go
+++ framework/file/file.go
@@ -11,7 +11,6 @@ func GetCurrentDirectory() string {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatalln(err)
-		os.Exit(1)
 	}
 	return strings.Replace(dir, "\\", "/", -1)
 }
